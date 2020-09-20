81ea296188e886619bcd056e4a870fed5e91f800
Isaac Kaaren <isaackaaren@gmail.com>
2020-09-19 19:57:48 -0600

    DEATHWALL
    Added finished sprites of the death wall, added animations, and collision/function that should end the game on contact with the player once we add the game over/ end game scene

--- Hazards/DeathWall.gd
+++ Hazards/DeathWall.gd
@@ -22,7 +22,11 @@ func stop_motion():
 	in_motion = false
 	
 	
+#
+#func _on_Area2D_body_entered(body):
+#	# End game
+#	pass # Replace with function body.
 
-func _on_Area2D_body_entered(body):
-	# End game
-	pass # Replace with function body.
+
+func _on_Area2D2_body_entered(body):
+	get_tree().call_group("Gamestate", "end_game")
